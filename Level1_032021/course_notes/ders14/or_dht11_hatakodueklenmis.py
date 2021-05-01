81bf1122f01aece5af40a7f68012069f8b78e3fd
H.SERIMER'den Elektronik Projeler <hserimer@gmail.com>
2021-05-01 15:17:40 +0300

    Create or_dht11_hatakodueklenmis.py

---
+++ Level1_032021/course_notes/ders14/or_dht11_hatakodueklenmis.py
@@ -0,0 +1,109 @@
+
+python kursu
+
+1. Çok kolay öğrenileniliyor
+2. ESP32 için elektronik bilgisinin olmasını gerekir
+3. Kurs iki aşamalı olmalı 1.başlangıç 2.ESP olmalı
+4.
+
+Ekran renkli olmalı 1.5 inc kadar
+
+PROJELER:
+    
+EV Enerji yönetimi kontrol sistemi
+AMPERMETRE
+VOLTMETRE
+Enerji güç tüketimi data kayıt cihazı (or: adafuruit)
+
+micropyton Büyük saat (220V led lambalar ile)
+Ege DENİZ 
+Duru Küçükkaya
+
+Albeza
+Cezeri EVCAR Telemetri sistemi
+
+Oğuzhan AYDIN
+Adafuruit.IO ile EV otomasyonu
+
+Mehmet Asaf AKDOĞAN
+micropython ESP32 ile Solar şarj cihazı
+
+Metin Hocam
+Micropyton kontrollü güç kaynağı
+
+
+Esma Rabia Özsaraç
+?
+
+
+Zahit Burhan
+ESP32 micropython OYUN konsolu yapımı
+
+
+Feyza YAREN
+ESP32 Micropython ile Sosyal mesafe sensörü
+
+Elif Yüce
+?
+
+Beril KEÇE
+Micropython ESP roket kontrol kartı tasarımı
+
+Ayşegül
+?
+
+Azra ARSLAN
+?
+
+Furkan BARUT
+?
+
+Fatih ADIBEKE
+Ahmet
+ESp32 Micropython 3d yazıcı ile smartwatch
+
+
+
+Zeynep Tuana
+?
+
+Elif Sena DOĞAN
+?
+
+İshak Burak CAN
+?
+
+Fatih Erdem Canpolat:
+ESP32 Micropython dron/ucak telemetri sistemi    
+    
+Koray CAN
+ESP32 Micropython Sualtı robot telemetri sistemi
+
+İbrahim hoca
+?
+
+Yasar hoca
+??
+
+Hüseyin hoca
+istiklal marşı cihazı ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
